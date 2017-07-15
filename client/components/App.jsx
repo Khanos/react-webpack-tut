386a89039208b81db5d43a36468b635cf1e04143
khanos <epilef.rodriguez@gmail.com>
2017-07-15 11:30:17 -0400

    Added axio library to fetch data from github api

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,7 +3,7 @@ import React, { PropTypes } from 'react'
 const Card = (props) => {
     return (
         <div style={{margin: '1em'}}>
-            <img width="75" src={props.avatarUrl} />
+            <img width="75" src={props.avatar_url} />
             <div className="info">
                 <div style={{fontSize: '1.25em', fontWeight: 'bold'}}>
                     {props.name}
@@ -24,7 +24,7 @@ const CardList = (props) => {
                     <Card
                         name={card.name}
                         company={card.company}
-                        avatarUrl={card.avatarUrl}
+                        avatar_url={card.avatar_url}
                     />
                 </div>
             )}
@@ -35,17 +35,25 @@ const CardList = (props) => {
 class Form extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            userName: ''
+        }
         this.handleSubmit = (event) => {
             event.preventDefault();
-            console.log('Event: Form submit', this.userNameInput.value);
-            console.log(this.userNameInput);
+            console.log('Event: Form submit', this.state.userName);
+            axios.get(`https://api.github.com/users/${this.state.userName}`)
+                .then(resp => {
+                    this.props.onSubmit(resp.data)
+                    this.setState({userName: ''});
+                });
         };
     };
     render () {
         return (
             <form onSubmit={this.handleSubmit}>
                 <input type="text" placeholder="Github username"
-                    ref={(input) => this.userNameInput = input  }
+                    value={this.state.userName}
+                    onChange={(event) => this.setState({ userName: event.target.value})}
                 required/>
                 <button type="submit">Add Card</button>
             </form>
@@ -57,25 +65,19 @@ class MainContainer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            cards: [
-                {
-                    id: 1,
-                    name: "Epilef Rodriguez",
-                    company: "Free Will",
-                    avatarUrl: "https://avatars3.githubusercontent.com/u/10603782?v=3"
-                },
-                {
-                    id: 2,
-                    name: "Angular",
-                    company: "null",
-                    avatarUrl: "https://avatars0.githubusercontent.com/u/139426?v=3"}
-            ]
+            cards: []
+        };
+        this.addNewCard = (cardInfo) => {
+            console.log(cardInfo);
+            this.setState(prevState => ({
+                cards: prevState.cards.concat(cardInfo)
+            }));
         };
     }
     render () {
         return (
             <div>
-                <Form />
+                <Form onSubmit={this.addNewCard} />
                 <CardList  cards={this.state.cards}/>
             </div>
         );
